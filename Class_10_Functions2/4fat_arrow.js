e8805fe5bd9534d1306aa72cc65476fef79b7e7d
SahilD1509 <sdhiman1509@gmail.com>
2025-07-14 10:20:44 +0530

    fatarrow

--- Class_10_Functions2/4fat_arrow.js
+++ Class_10_Functions2/4fat_arrow.js
@@ -1,9 +1,19 @@
-function wish() {
+/* function wish() {
     console.log("GM")
 }
 wish()
 
 function wish(){ 
     console.log("GN")
+}
+wish() */
+
+var wish = () => {
+    console.log("Good morning")
+}
+wish()
+
+var wish = () => {
+    console.log("Good night")
 }
 wish()
