dc65461aa43f0bc89e29c26666b1f2ef53b55c5c
SahilD1509 <sdhiman1509@gmail.com>
2025-07-31 10:33:20 +0530

    Asynchronous

---
+++ Class_21_Asychronous/4callback.js
@@ -0,0 +1,12 @@
+setTimeout(()=>{
+    console.log("Req R-1")
+},2000)
+setTimeout(()=>{
+    console.log("Req R-2")
+},8000)
+setTimeout(()=>{
+    console.log("Req R-3")
+},3000)
+setTimeout(()=>{
+    console.log("Req R-4")
+},4000)
